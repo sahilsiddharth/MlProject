afb864b22027a05e0e59c133b618491b0b4108c2
sahilsiddharth <sahilsiddharth@gmail.com>
2025-01-02 18:38:02 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os 
 import sys
+from src.components.data_transformation import DataTransformation
 from src.exception import CustomException
 from src.logger import logging
 
@@ -41,4 +42,6 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
